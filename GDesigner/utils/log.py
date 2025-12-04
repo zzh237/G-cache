209c63586cf304e8ae8c960f183cb0e1c8decadb
Wei Wan <bleachvex@Weis-MacBook-Pro.local>
2025-12-04 14:06:28 -0800

    new

---
+++ GDesigner/utils/log.py
@@ -0,0 +1,8 @@
+"""Simple logger placeholder"""
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
